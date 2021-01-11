bf3b8860d0153845c4ae4cd2e67df08b81feaf4d
A Code Fox <heyacfox@gmail.com>
2021-01-11 11:03:31 -0500

    Imported Tilt Brush Tools

---
+++ Assets/Behaviours/CameraClearFlagsBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraClearFlagsBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Camera>().clearFlags = CameraClearFlags.Nothing;
+    }
+
+    
+}
---
+++ Assets/Behaviours/SkyboxBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxBehaviour : MonoBehaviour
+{
+    //Code https://answers.unity.com/questions/651780/rotate-skybox-constantly.html
+    public float speedMultiplier = 1f;
+
+    private void Start()
+    {
+        GetComponent<Camera>().clearFlags = CameraClearFlags.Nothing;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * speedMultiplier);
+    }
+}
