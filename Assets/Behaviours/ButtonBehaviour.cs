9ae360f7931a7d122b0738da0fc1ecb8227c0d7f
Autoquark <github@autoquark.33mail.com>
2021-01-12 19:13:36 +0000

    Button that triggers callback when pushed in by a ball

---
+++ Assets/Behaviours/ButtonBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Assets.Behaviours
+{
+    class ButtonBehaviour : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject _button;
+        [SerializeField]
+        private UnityEvent _onPushed;
+
+        private bool _triggered;
+
+        private void Update()
+        {
+            if(!_triggered && _button.transform.localPosition.y <= 0.21)
+            {
+                _triggered = true;
+                _onPushed.Invoke();
+            }
+        }
+    }
+}
