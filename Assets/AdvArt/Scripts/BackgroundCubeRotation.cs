3faa2194852695f242beefa395e1defed5bf30c3
A Code Fox <heyacfox@gmail.com>
2021-01-13 15:23:40 -0500

    Removing Tiltbrush

---
+++ Assets/AdvArt/Scripts/BackgroundCubeRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundCubeRotation : MonoBehaviour
+{
+    public float maxTorque = 100000f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float randomX = Random.Range(0f, maxTorque);
+        float randomY = Random.Range(0f, maxTorque);
+        float randomZ = Random.Range(0f, maxTorque);
+        GetComponent<Rigidbody>().AddTorque(new Vector3(randomX, randomY, randomZ));
+    }
+
+    
+}
