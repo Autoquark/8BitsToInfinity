2b0ca979c1930c4e08cd5cf6e5cd6a3672f7adf7
A Code Fox <heyacfox@gmail.com>
2021-01-15 11:01:21 -0500

    Added audio sounds, and a background audio prefab

---
+++ Assets/Sound/SoundScripts/CrossSceneAudio.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrossSceneAudio : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    public void PlayAudio(AudioClip audioClip)
+    {
+        DontDestroyOnLoad(gameObject);
+        audioSource.PlayOneShot(audioClip);
+    }
+
+    private void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
