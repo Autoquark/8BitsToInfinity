e46ffb7734c32adc4d4fe690853eaca15d9c74c5
Ian Badcoe <ian_badcoe@yahoo.co.uk>
2021-01-17 16:00:53 +0000

    Keys for level complete menu functions

--- Assets/Behaviours/Ui/LevelCompleteMenuBehaviour.cs
+++ Assets/Behaviours/Ui/LevelCompleteMenuBehaviour.cs
@@ -23,13 +23,35 @@ namespace Assets.Behaviours.Ui
         private void Start()
         {
             _restartLevelButton.onClick.AddListener(() => _levelController.RestartLevel());
-            _nextLevelButton.onClick.AddListener(() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex == LevelData._lastLevelIndex ? 1 : SceneManager.GetActiveScene().buildIndex + 1));
-            _mainMenuButton.onClick.AddListener(() => SceneManager.LoadScene("Level_Machine"));
+            _nextLevelButton.onClick.AddListener(NextLevel);
+            _mainMenuButton.onClick.AddListener(MainMenu);
+        }
+
+        private static void MainMenu()
+        {
+            SceneManager.LoadScene("Level_Machine");
+        }
+
+        private static void NextLevel()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex == LevelData._lastLevelIndex ? 1 : SceneManager.GetActiveScene().buildIndex + 1);
         }
 
         private void OnEnable()
         {
             //_nextLevelButton.gameObject.SetActive(SceneManager.GetActiveScene().buildIndex != LevelData._lastLevelIndex);
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.N))
+            {
+                NextLevel();
+            }
+            if (Input.GetKeyDown(KeyCode.M))
+            {
+                MainMenu();
+            }
         }
     }
 }
